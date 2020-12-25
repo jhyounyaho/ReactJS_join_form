6c6c921838580b5c27b7a6ce58b9e13e14cb4c45
jhyounyaho <https://github.com/jhyounyaho>
2020-12-25 10:24:10 +0900

    회원가입 폼 1차 수정 및 가입요소 추가

--- src/Components/JoinForm/JoinForm.js
+++ src/Components/JoinForm/JoinForm.js
@@ -80,24 +80,45 @@ function JoinForm() {
   const [sex, setSex] = useState('');
   const [sexError, setSexError] = useState(false);
 
-  const [birth, setBirth] = useState('');
-  const [birthError, setBirthError] = useState(false);
+  const [birthYear, setBirthYear] = useState('')
+  const [birthYearError, setBirthYearError] = useState(false);
+  let yearSelect = [];
+  for(let i = 2020; i > 1920; i--) {
+    yearSelect.push(<option value={i}>{i}</option>)
+  }
+
+  const [birthMonth, setBirthMonth] = useState('')
+  const [birthMonthError, setBirthMonthError] = useState(false);
+  let monthSelect = [];
+  for(let i = 1; i <= 12; i++) {
+    monthSelect.push(<option value={i}>{i}</option>)
+  }
+
+  const [birthDay, setBirthDay] = useState('')
+  const [birthDayError, setBirthDayError] = useState(false);
+  let daySelect = [];
+  for(let i = 1; i <= 31; i++) {
+    daySelect.push(<option value={i}>{i}</option>)
+  }
+
+  const [exInfo, setExInfo] = useState('');
+  const [exInfoError, setExInfoError] = useState(false);
 
   const onSubmit = (e) => {
     e.preventDefault();
     // 유효성체크
   }
 
-  // 유효성 체크 - ID 
+  // 유효성 체크 - ID (4~12자의 영문과 숫자 허용) 
   const onChangeId = (e) => {
-    const regExp = /^[a-zA-Z0-9]{4,12}$/; // 4~12자의 영문과 숫자 허용
+    const regExp = /^[a-zA-Z0-9]{4,12}$/;  
     setIdError(!regExp.test(e.target.value))
     setId(e.target.value);
   }
 
-  // 유효성 체크 - 비밀번호 
+  // 유효성 체크 - 비밀번호 (4~12자의 영문과 숫자 허용) 
   const onChangePassword = (e) => {
-    const regExp = /^[a-zA-Z0-9]{4,12}$/; // 4~12자의 영문과 숫자 허용
+    const regExp = /^[a-zA-Z0-9]{4,12}$/;  
     setPasswordError(!regExp.test(e.target.value))
     setPassword(e.target.value);
   }
@@ -108,33 +129,46 @@ function JoinForm() {
     setPasswordCheck(e.target.value);
   }
 
-  // 유효성 체크 - 영문 이름 
+  // 유효성 체크 - 영문 이름 (2~20자의 영문과 공백 허용) 
   const onChangeMiddleName = (e) => {
-    const regExp = /^[a-zA-Z\s]{2,20}$/; // 2~20자의 영문과 공백 허용
+    const regExp = /^[a-zA-Z\s]{2,20}$/;  
     setMiddleNameError(!regExp.test(e.target.value));
     setMiddleName(e.target.value);
   }
 
-  // 유효성 체크 - 영문 성 
+  // 유효성 체크 - 영문 성 (2~20자의 영문과 공백 허용) 
   const onChangeFirstName = (e) => {
-    const regExp = /^[a-zA-Z\s]{2,20}$/; // 2~20자의 영문과 공백 허용
+    const regExp = /^[a-zA-Z\s]{2,20}$/;  
     setFirstNameError(!regExp.test(e.target.value));
     setFirstName(e.target.value);
   }
 
-  // 유효성 체크 - 한글 이름 
+  // 유효성 체크 - 한글 이름 (2~20자의 한글만 허용) 
   const onChangeKoName = (e) => {
-    const regExp = /^[가-힣]{2,20}$/; // 2~20자의 한글만 허용
+    const regExp = /^[가-힣]{2,20}$/;  
     setKoNameError(!regExp.test(e.target.value));
     setKoName(e.target.value);
   }
 
-  // 유효성 체크 - 생년월일  
-  const onChangeBirth = (e) => {
-    const regExp = /^[0-9]{0,6}$/; // 숫자만 허용
-    setBirthError(!regExp.test(e.target.value));
-    setBirth(e.target.value);
-  } 
+  const onChangeBirthYear = (e) => {
+    setBirthYearError(e.target.value === '')
+    setBirthYear(e.target.value);
+  }
+
+  const onChangeBirthMonth = (e) => {
+    setBirthMonthError(e.target.value === '')
+    setBirthMonth(e.target.value);
+  }
+
+  const onChangeBirthDay = (e) => {
+    setBirthDayError(e.target.value === '')
+    setBirthDay(e.target.value);
+  }
+
+  const onChangeExInfo = (e) => {
+    setExInfoError(e.target.value.length > 20);
+    setExInfo(e.target.value);
+  }
 
   return (
     <div>
@@ -143,88 +177,43 @@ function JoinForm() {
         <label for='user_id'>
           <p>ID</p>
           <div>
-            <input 
-              type='text' 
-              name='user_id' 
-              value={id} 
-              onChange={onChangeId} 
-            />
-            {
-              idError && (<p className='is-invalid'>4~12자의 영문 대소문자와 숫자로만 입력 가능합니다.</p>)
-            }
+            <input type='text' name='user_id' value={id} onChange={onChangeId} />
+            { idError && (<p className='is-invalid'>4~12자의 영문 대소문자와 숫자로만 입력 가능합니다.</p>) }
           </div>
         </label>
         <label for='user_password'>
           <p>비밀번호</p>
           <div>
-            <input 
-              type='password' 
-              name='user_password' 
-              value={password} 
-              onChange={onChangePassword} 
-            />
-            {
-              passwordError && (<p className='is-invalid'>4~12자의 영문 대소문자와 숫자로만 입력 가능합니다.</p>)
-            }
+            <input type='password' name='user_password' value={password} onChange={onChangePassword} />
+            { passwordError && (<p className='is-invalid'>4~12자의 영문 대소문자와 숫자로만 입력 가능합니다.</p>) }
           </div>
         </label>
         <label for='user_password_check'>
           <p>비밀번호 확인</p>
           <div>
-            <input 
-              type='password' 
-              name='user_password_check' 
-              value={passwordCheck} 
-              onChange={onChangePasswordCheck} 
-            />
-            {
-              passwordCheckError && (<p className='is-invalid'>비밀번호가 일치하지 않습니다.</p>)
-            }
+            <input type='password' name='user_password_check' value={passwordCheck} onChange={onChangePasswordCheck} />
+            { passwordCheckError && (<p className='is-invalid'>비밀번호가 일치하지 않습니다.</p>) }
           </div>
         </label>
         <label for='user_middle_name'>
           <p>영문 이름</p>
           <div>
-            <input 
-              type='text' 
-              name='user_middle_name' 
-              placeholder='Gil Dong'
-              value={middleName} 
-              onChange={onChangeMiddleName} 
-            />
-            {
-              middleNameError && (<p className='is-invalid'>2~20자의 영문 대소문자와 공백만 입력 가능합니다.</p>)
-            }
+            <input type='text' name='user_middle_name' placeholder='Gil Dong' value={middleName} onChange={onChangeMiddleName} />
+            { middleNameError && (<p className='is-invalid'>2~20자의 영문 대소문자와 공백만 입력 가능합니다.</p>) }
           </div>
         </label>
         <label for='user_first_name'>
           <p>영문 성</p>
           <div>
-            <input 
-              type='text' 
-              name='user_first_name' 
-              placeholder='Hong'
-              value={firstName} 
-              onChange={onChangeFirstName} 
-            />
-            {
-              firstNameError && (<p className='is-invalid'>2~20자의 영문 대소문자와 공백만 입력 가능합니다.</p>)
-            }
+            <input type='text' name='user_first_name' placeholder='Hong' value={firstName} onChange={onChangeFirstName} />
+            { firstNameError && (<p className='is-invalid'>2~20자의 영문 대소문자와 공백만 입력 가능합니다.</p>) }
           </div>
         </label>
         <label for='user_ko_name'>
           <p>한글 이름</p>
           <div>
-            <input 
-              type='text' 
-              name='user_ko_name' 
-              placeholder='홍길동'
-              value={koName} 
-              onChange={onChangeKoName} 
-            />
-            {
-              koNameError && (<p className='is-invalid'>2~20자의 한글만 입력 가능합니다.</p>)
-            }
+            <input type='text' name='user_ko_name' placeholder='홍길동' value={koName} onChange={onChangeKoName} />
+            { koNameError && (<p className='is-invalid'>2~20자의 한글만 입력 가능합니다.</p>) }
           </div>
         </label>
         <label for='user_sex'>
@@ -232,46 +221,49 @@ function JoinForm() {
           <ul>
             <li>
               <div>
-                <input 
-                  type='radio' 
-                  name='user_sex' 
-                  value='male' 
-                  checked={sex === 'male'}
-                  onClick={() => setSex('male')}
-                />
+                <input type='radio' name='user_sex' value='male' checked={sex === 'male'} onClick={() => setSex('male')} />
                 <span>남</span>
               </div>
             </li>
             <li>
               <div>
-                <input 
-                  type='radio' 
-                  name='user_sex' 
-                  value='female' 
-                  checked={sex === 'female'}
-                  onClick={() => setSex('female')}
-                />
+                <input type='radio' name='user_sex' value='female' checked={sex === 'female'} onClick={() => setSex('female')} />
                 <span>여</span>
               </div>
             </li>
           </ul>
-          {
-            sexError && (<p className='is-invalid'>{}</p>)
-          }
+          { sexError && (<p className='is-invalid'>{}</p>) }
         </label>
-        <label for='user_birth'>
-          <p>생년월일</p>  
+        생일
+        <div>
+          <label for='birth_year'>
+            <select name='birth_year' value={birthYear} onChange={onChangeBirthYear}>
+              <option value=''>YYYY</option>
+              {yearSelect.map(e => e)}
+            </select>
+          </label>       
+          년 
+          <label for='birth_month'>
+            <select name='birth_month' value={birthMonth} onChange={onChangeBirthMonth}>
+              <option value=''>MM</option>
+              {monthSelect.map(e => e)}
+            </select>
+          </label>       
+          월 
+          <label for='birth_day'>
+            <select name='birth_day' value={birthDay} onChange={onChangeBirthDay}>
+              <option value=''>DD</option>
+              {daySelect.map(e => e)}
+            </select>
+          </label>       
+          일 
+          { (birthYearError || birthMonthError || birthDayError) && (<p className='is-invalid'>생년월일을 입력해 주세요</p>) }
+        </div>
+        <label for='ex_info'>
+          <p>문의사항</p>  
           <div>
-            <input 
-              type='text' 
-              name='user_birth' 
-              placeholder='YYMMDD'
-              value={birth}
-              onChange={onChangeBirth} 
-            />
-            {
-              birthError && (<p className='is-invalid'>6자리의 생년월일을 입력해 주세요.(YYMMDD)</p>)
-            }
+            <textarea name='ex_info' value={exInfo} placeholder='문의사항을 입력해 주세요.' onChange={onChangeExInfo} />
+            { exInfoError && (<p className='is-invalid'>200자 이하로 입력해 주세요.</p>) }
           </div>
         </label>
         <div>
